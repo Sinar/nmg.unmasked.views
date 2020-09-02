ede0f6aa758c6394e603c06dca729852e24c2de0
Khairil Yusof <khairil.yusof@gmail.com>
2020-09-02 18:44:59 +0800

    "Add view: nmg-person-listing"

---
+++ src/nmg/unmasked/views/views/n_m_g_person_listing.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from nmg.unmasked.views import _
+from plone.dexterity.browser.view import DefaultView
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class NMGPersonListing(DefaultView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('nmg_person_listing.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return super(NMGPersonListing, self).__call__()
