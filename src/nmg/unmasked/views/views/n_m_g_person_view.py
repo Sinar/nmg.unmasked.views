9d7edf228d8efa2ddf58a796ce9fea94ed8a8bc3
Khairil Yusof <khairil.yusof@gmail.com>
2020-09-03 20:17:27 +0800

    Add NMG Person Views

--- src/nmg/unmasked/views/views/n_m_g_person_view.py
+++ src/nmg/unmasked/views/views/n_m_g_person_view.py
@@ -3,6 +3,13 @@
 from nmg.unmasked.views import _
 from plone.dexterity.browser.view import DefaultView
 
+from Acquisition import aq_inner
+from zope.component import getUtility
+from zope.intid.interfaces import IIntIds
+from zope.security import checkPermission
+from zc.relation.interfaces import ICatalog
+from zope.schema.interfaces import IVocabularyFactory
+
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
@@ -14,3 +21,157 @@ class NMGPersonView(DefaultView):
     def __call__(self):
         # Implement your own actions:
         return super(NMGPersonView, self).__call__()
+
+    def memberships(self):
+        """
+        Return back references from source object on specified attribute_name
+        """
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
+
+        source_object = self.context
+        attribute_name = 'person'
+
+        result = []
+
+        for rel in catalog.findRelations(
+            dict(to_id=intids.getId(aq_inner(source_object)),
+                 from_attribute=attribute_name)
+              ):
+            
+            obj = intids.queryObject(rel.from_id)
+
+            if obj is not None and checkPermission('zope2.View', obj):
+                if obj.portal_type == 'Membership':
+                    result.append(obj)
+
+        return result
+
+    def relationships_subject(self):
+        # Get relationships where person or organization is the subject
+
+        """
+        Return back references from source object on specified attribute_name
+        """
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
+
+        source_object = self.context
+
+        result = []
+
+        for rel in catalog.findRelations(
+            dict(to_id=intids.getId(aq_inner(source_object)),
+                 from_attribute='relationship_subject')
+              ):
+
+            obj = intids.queryObject(rel.from_id)
+
+            if obj is not None and checkPermission('zope2.View', obj):
+                result.append(obj)
+
+        return result
+
+    def relationships_object(self):
+        # Get relationships where person or organization is the object
+
+        """
+        Return back references from source object on specified attribute_name
+        """
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
+
+        source_object = self.context
+
+        result = []
+
+        for rel in catalog.findRelations(
+            dict(to_id=intids.getId(aq_inner(source_object)),
+                 from_attribute='relationship_object')
+              ):
+
+            obj = intids.queryObject(rel.from_id)
+
+            if obj is not None and checkPermission('zope2.View', obj):
+                result.append(obj)
+
+        return result
+
+    def relationship_title(self, value):
+
+        factory = getUtility(IVocabularyFactory,
+                             'popolo.contenttypes.relationshiptypes')
+
+        vocabulary = factory(self)
+
+        term = vocabulary.getTerm(value)
+
+        return term.title
+
+    def stories(self):
+        """
+        Return back references from source object where
+
+        """
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
+
+        source_object = self.context
+        attribute_names = ['implicated', 'directly_implicated',
+                           'circumstantially_implicated']
+
+        result = []
+
+        for attribute_name in attribute_names:
+
+            for rel in catalog.findRelations(
+                dict(to_id=intids.getId(aq_inner(source_object)),
+                     from_attribute=attribute_name)
+                  ):
+
+                obj = intids.queryObject(rel.from_id)
+
+                if obj is not None and checkPermission('zope2.View', obj):
+                    if obj.portal_type == 'Issue Source':
+                        result.append(obj)
+
+        unique = list(dict.fromkeys(result))
+
+        return unique
+
+    def contracts(self):
+        """
+        Return back references from source object where
+
+        """
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
+
+        source_object = self.context
+        attribute_names = ['buyer',
+                           'procuringEntity',
+                           'administrativeEntity',
+                           'suppliers',
+                           'tenderers',
+                           'funders',
+                           'reviewBody',
+                           'interestedParties',
+                           ]
+
+        result = []
+
+        for attribute_name in attribute_names:
+
+            for rel in catalog.findRelations(
+                dict(to_id=intids.getId(aq_inner(source_object)),
+                     from_attribute=attribute_name)
+                  ):
+
+                obj = intids.queryObject(rel.from_id)
+
+                if obj is not None and checkPermission('zope2.View', obj):
+                    result.append(obj)
+
+        unique = list(dict.fromkeys(result))
+
+        return unique
