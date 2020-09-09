d660a5946db2d19008c6f6546773a089cfd156b0
Khairil Yusof <khairil.yusof@gmail.com>
2020-09-09 18:43:43 +0800

    Initial procurement view

--- src/nmg/unmasked/views/views/n_m_g_procurement_view.py
+++ src/nmg/unmasked/views/views/n_m_g_procurement_view.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from Acquisition import aq_inner
+from zope.component import getUtility
+from zope.intid.interfaces import IIntIds
+from zope.security import checkPermission
+from zc.relation.interfaces import ICatalog
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from zope.schema.interfaces import IVocabularyFactory
+
 from nmg.unmasked.views import _
 from Products.Five.browser import BrowserView
 
@@ -15,3 +23,24 @@ class NMGProcurementView(BrowserView):
         # Implement your own actions:
         self.msg = _(u'A small message')
         return self.index()
+
+    def awards(self):
+        brains = self.context.getFolderContents(contentFilter={
+            'portal_type': ['Award']})
+
+        objs = []
+        for obj in brains:
+            objs.append(obj.getObject())
+
+        return objs
+
+    def directors(self, folder):
+
+        memberships = folder.listFolderContents(contentFilter={
+            'portal_type': ['Membership']})
+
+        directors = []
+        for membership in memberships:
+            directors.append(membership.person.to_object)
+
+        return directors
