905d746f858d013a2860d7038fe95bdaa321ce7c
Khairil Yusof <khairil.yusof@gmail.com>
2020-09-09 12:02:20 +0800

    Initial Issue Source (Story) and Issue view

--- src/nmg/unmasked/views/views/n_m_g_issue_view.py
+++ src/nmg/unmasked/views/views/n_m_g_issue_view.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from Acquisition import aq_inner
+from zope.component import getUtility
+from zope.intid.interfaces import IIntIds
+from zope.security import checkPermission
+from zc.relation.interfaces import ICatalog
+from zope.schema.interfaces import IVocabularyFactory
+
+from operator import methodcaller
+
 from nmg.unmasked.views import _
 from Products.Five.browser import BrowserView
 
@@ -11,7 +20,75 @@ class NMGIssueView(BrowserView):
     # the configure.zcml registration of this view.
     # template = ViewPageTemplateFile('nmg_issue_view.pt')
 
-    def __call__(self):
-        # Implement your own actions:
-        self.msg = _(u'A small message')
-        return self.index()
+    def contracts(self):
+        """
+        Return back references from source object where
+
+        """
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
+
+        source_object = self.context
+        attribute_names = ['buyer',
+                           'procuringEntity',
+                           'administrativeEntity',
+                           'suppliers',
+                           'tenderers',
+                           'funders',
+                           'reviewBody',
+                           'interestedParties',
+                           ]
+
+        result = []
+
+        for attribute_name in attribute_names:
+
+            for rel in catalog.findRelations(
+                dict(to_id=intids.getId(aq_inner(source_object)),
+                     from_attribute=attribute_name)
+                  ):
+
+                obj = intids.queryObject(rel.from_id)
+
+                if obj is not None and checkPermission('zope2.View', obj):
+                    result.append(obj)
+
+        unique = list(dict.fromkeys(result))
+
+        return unique
+
+    def stories(self):
+        """
+        Return back references from issue source to issue
+
+        """
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
+
+        source_object = self.context
+        attribute_names = ['issue']
+
+        result = []
+
+        for attribute_name in attribute_names:
+
+            for rel in catalog.findRelations(
+                dict(to_id=intids.getId(aq_inner(source_object)),
+                     from_attribute=attribute_name)
+                  ):
+
+                obj = intids.queryObject(rel.from_id)
+
+                if obj is not None and checkPermission('zope2.View', obj):
+                    if obj.portal_type == 'Issue Source':
+                        result.append(obj)
+
+        unique = list(dict.fromkeys(result))
+
+        sorted_effective = sorted(unique,
+                                  key=methodcaller('effective'),
+                                  reverse=True)
+        if sorted_effective:
+            return sorted_effective
+        else:
+            return None
