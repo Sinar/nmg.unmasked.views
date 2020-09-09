046c300cfe711eae97303a4e3fbdfe436b79a32a
Khairil Yusof <khairil.yusof@gmail.com>
2020-09-09 21:35:06 +0800

    Initial front page custom view

--- src/nmg/unmasked/views/views/unmasked_front_page_view.py
+++ src/nmg/unmasked/views/views/unmasked_front_page_view.py
@@ -3,6 +3,10 @@
 from nmg.unmasked.views import _
 from Products.Five.browser import BrowserView
 
+from operator import methodcaller
+
+from plone import api
+
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
@@ -11,7 +15,28 @@ class UnmaskedFrontPageView(BrowserView):
     # the configure.zcml registration of this view.
     # template = ViewPageTemplateFile('unmasked_front_page_view.pt')
 
-    def __call__(self):
-        # Implement your own actions:
-        self.msg = _(u'A small message')
-        return self.index()
+    def stories(self):
+        # 5 latest published stories
+        stories = api.content.find(portal_type='Issue Source',
+                                   sort_on='effective',
+                                   sort_order='descending',
+                                   )[:5]
+
+        return stories
+
+    def first_story(self):
+        # 5 latest published stories
+        stories = api.content.find(portal_type='Issue Source',
+                                   sort_on='effective',
+                                   sort_order='descending',
+                                   )
+
+        return stories[0]
+
+    def issues(self):
+        # 5 latest published stories
+        issues = api.content.find(portal_type='Issue',
+                                  sort_on='effective',
+                                  sort_order='descending')[:3]
+
+        return issues
